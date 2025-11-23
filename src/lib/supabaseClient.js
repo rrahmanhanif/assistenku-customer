c32966cc286071024cf60d0b59cbf8d8de6b135b
rrahmanhanif <rrahmanhanif@gmail.com>
2025-11-23 15:54:23 +0700

    Update supabaseClient.js

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,15 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 
-// Gunakan ENV versi Vite
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-// Validasi agar tidak error saat npm run dev
 if (!supabaseUrl || !supabaseAnonKey) {
-  console.error("❌ ENV Supabase tidak ditemukan!");
-  console.error("Pastikan .env sudah dibuat dan berisi:");
-  console.error("VITE_SUPABASE_URL=....");
-  console.error("VITE_SUPABASE_ANON_KEY=....");
+  console.error("❌ ENV Supabase tidak ditemukan! Cek file .env Anda");
 }
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
