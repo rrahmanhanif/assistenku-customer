9dfee3fe4b1104843c381833a232ca4a4e51d74e
rrahmanhanif <rrahmanhanif@gmail.com>
2025-11-23 15:38:19 +0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,6 @@
-// Import Firebase
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
-// Konfigurasi Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyDDw8VRXSvjGuMgKsEvbufLmXyGgI4hu7U",
   authDomain: "assistenku-customer.firebaseapp.com",
@@ -10,10 +8,9 @@ const firebaseConfig = {
   storageBucket: "assistenku-customer.firebasestorage.app",
   messagingSenderId: "1021599386974",
   appId: "1:1021599386974:web:7350342a375509707d93cf",
-  measurementId: "G-813KN9V58E"
+  measurementId: "G-813KN9V58E",
 };
 
-// Inisialisasi Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
