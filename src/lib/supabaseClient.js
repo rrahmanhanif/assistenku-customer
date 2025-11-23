049ea8b1d2c158e2a71d67d0b9eb4379755987df
rrahmanhanif <rrahmanhanif@gmail.com>
2025-11-23 20:14:31 +0700

    Update supabaseClient.js

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,10 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-
-if (!supabaseUrl || !supabaseAnonKey) {
-  console.error("❌ ENV Supabase tidak ditemukan! Cek file .env Anda");
-}
+const supabaseUrl = "https://vptfubypmfafrnmwweyj.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc...zmw";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
