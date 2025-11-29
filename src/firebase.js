b4a870f3147915a911b2256ac31e59b74616f0eb
rrahmanhanif <rrahmanhanif@gmail.com>
2025-11-29 12:42:11 +0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,18 @@
+// src/firebase.js
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import {
+  getFirestore
+} from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBSL87qkuwSQU8aXvLuu24nV7jUoX2mOSA",
   authDomain: "assistenku-8ef85.firebaseapp.com",
   projectId: "assistenku-8ef85",
   storageBucket: "assistenku-8ef85.appspot.com",
-  messagingSenderId: "1090989610821",
-  appId: "1:1090989610821:web:yourappid",
+  messagingSenderId: "550042793325",
+  appId: "1:550042793325:web:deaf9c950e7e3f9534aabe"
 };
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-export const auth = getAuth(app);
+export default app;
