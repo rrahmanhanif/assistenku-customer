7c3a8936d545c6d4d12a51ed56175c6e9aa6a082
rrahmanhanif <rrahmanhanif@gmail.com>
2025-11-29 23:45:07 +0700

    Create realtimecustomer.js

---
+++ src/lib/realtimecustomer.js
@@ -0,0 +1,21 @@
+import { createClient } from "@supabase/supabase-js";
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+);
+
+export function subscribeOrderStatus(orderId, callback) {
+  return supabase
+    .channel(`order-status-${orderId}`)
+    .on(
+      "postgres_changes",
+      {
+        event: "*",
+        schema: "public",
+        table: "orders",
+        filter: `id=eq.${orderId}`,
+      },
+      (payload) => callback(payload.new)
+    )
+    .subscribe();
+}
