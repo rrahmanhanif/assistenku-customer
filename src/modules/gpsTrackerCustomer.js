4ecaf54fac6e0486080bfd8b051ab5b1ee2e9cb7
rrahmanhanif <rrahmanhanif@gmail.com>
2025-11-29 12:37:47 +0700

    Update gpsTrackerCustomer.js

--- src/modules/gpsTrackerCustomer.js
+++ src/modules/gpsTrackerCustomer.js
@@ -2,9 +2,9 @@ import { db } from "../firebase";
 import { doc, updateDoc } from "firebase/firestore";
 
 export async function updateCustomerLocation(uid, lat, lng) {
-  await setDoc(
-    doc(db, "customer_locations", uid),
-    { lat, lng, updatedAt: Date.now() },
-    { merge: true }
-  );
+await updateDoc(doc(db, "gps_customer", userId), {
+  lat,
+  lng,
+  updatedAt: Date.now()
+});
 }
