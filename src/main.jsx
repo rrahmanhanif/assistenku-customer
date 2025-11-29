2b7333ce173837772e5be26d3b68555ef1fa3ea1
rrahmanhanif <rrahmanhanif@gmail.com>
2025-11-29 13:02:45 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
-import 'leaflet/dist/leaflet.css';
 import React from "react";
 import ReactDOM from "react-dom";
+import 'leaflet/dist/leaflet.css';
 import App from "./App";
 import "./index.css"; // ✅ wajib sesuai
