4332979b28d14ff84c004dce90b17323178ed8a6
rrahmanhanif <rrahmanhanif@gmail.com>
2025-11-30 08:02:12 +0700

    Create notification.js

---
+++ src/modules/notification.js
@@ -0,0 +1,29 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+  "https://vptfubypmfafrnmwweyj.supabase.co",
+  "YOUR_ANON_KEY"
+);
+
+// callback akan dipanggil ketika ada pesan masuk dari mitra
+export function listenCustomerNotification(customerId, callback) {
+  return supabase
+    .channel(`notif-customer-${customerId}`)
+    .on(
+      "postgres_changes",
+      {
+        event: "INSERT",
+        schema: "public",
+        table: "messages",
+        filter: `sender=eq.mitra`
+      },
+      (payload) => {
+        callback({
+          order_id: payload.new.order_id,
+          message: payload.new.message,
+          sender: payload.new.sender
+        });
+      }
+    )
+    .subscribe();
+}
