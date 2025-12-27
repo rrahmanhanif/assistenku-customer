3606b9aae625b0446e8593fbbfbbc398bb657d41
rrahmanhanif <rrahmanhanif@gmail.com>
2025-12-27 19:26:01 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { initializeRuntimeLogging } from "./app/runtimeLogger";
 
 import "leaflet/dist/leaflet.css";
+
+const cleanupLogging = initializeRuntimeLogging();
+
+if (import.meta.hot) {
+  import.meta.hot.dispose(() => {
+    cleanupLogging();
+  });
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
